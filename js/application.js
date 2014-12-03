d2e8e5787fa19dee700f85d90a402433c46632c4
cchchoi1986 <cchchoi1986@gmail.com>
2014-12-03 14:47:03 +0800

    added symbol generator, sq and sqrt tickers, and css styles

--- js/application.js
+++ js/application.js
@@ -1,60 +1,141 @@
-$(document).ready(function(){
+var timeLeft;
+var stripp;
+var qBank;
+var nextLevel;
+var timer;
+var highScore;
 
-  $(function() {
+$(document).ready(function(){
+  $('#inputs .input-bar').hide();
+  { //Slider
     $( "#slider-range-max" ).slider({
       range: "max",
       min: 10,
       max: 100,
-      value: 10,
+      value: 20,
       slide: function( event, ui ) {
         $( "#amount" ).val( ui.value );
       }
     });
     $( "#amount" ).val( $( "#slider-range-max" ).slider( "value" ) );
-  });
-
-  var timeLeft = 10;
-
-  var stripp = function(x){
-    return x.replace('+"+"','');
-  }
-  
-  $(document).on('click', '.start-button', function(){
-    timeLeft = 10;
-    $('#inputs .start-button').remove();
-    var timer = setInterval(function(){
-      if (timeLeft >= 0) {
-        $('#timer').text(timeLeft);
-        console.log(timeLeft);
-        timeLeft--;
-      }
-      else {
-        clearInterval(timer);
-        $('.input-bar').after('<button class="start-button">Click to Play</button>');
-      }
-    }, 1000)
-  });
-
-  var rando = function(){
-    return (Math.random()*$('#amount').val()).toFixed(0)+"+"+(Math.random()*$('#amount').val()).toFixed(0);
   }
 
-  var qBank = function(){
-    var ask = rando();
-    $('#questions').text(ask);
-    $(document).keyup(function(){
-      if ($('.input-bar').val() == eval(stripp(ask))){
-        timeLeft++;
-        ask = rando();
-        $('#questions').text(ask);
-        $('.input-bar').val("");
+  { //Strip function
+    // stripp = function(x){
+    //   return x.replace('+"+"+','+');
+    //   // x = x.replace('+"-"+','-');
+    //   // x = x.replace('+"x"+','*');
+    //   // x = x.replace('+"/"+','/');
+    //   // return x;
+    // };
+
+    stripp = function(input){
+      var output = input;
+      output = output.replace(' x ', '*');
+      output = output.replace(' ÷ ', '/');
+      return output;
+    };
+  }
+
+  { //Question bank
+    qBank = function(){
+      var choices = [$('#add'),$('#subtract'),$('#multiply'),$('#divide'),$('#squared'),$('#sqroot')]
+      var ticked = [];
+      var i =0;
+      while (i < choices.length) {
+        if (choices[i].prop("checked") == true){
+          ticked.push(choices[i].data('symbol'));
+          i++;
+        }
+        else {
+          i++;
+        }
       }
-      else {
-        console.log('web dev fail');
+      var rando = (Math.random()*(ticked.length-1)).toFixed(0);
+      var choiceSymbol;
+      switch(ticked[rando]){
+        case 'add':
+          return (Math.random()*$('#amount').val()).toFixed(0) + ' + ' + (Math.random()*$('#amount').val()).toFixed(0); 
+          break;
+        case 'subtract':
+          return (Math.random()*$('#amount').val()).toFixed(0)+" - "+(Math.random()*$('#amount').val()).toFixed(0);
+          break;
+        case 'multiply':
+          return (Math.random()*$('#amount').val()).toFixed(0)+" x "+(Math.random()*$('#amount').val()).toFixed(0);
+          break;
+        case 'divide':
+          var num1 = Math.floor(Math.random()*Math.sqrt($('#amount').val()));
+          var num2 = Math.floor(Math.random()*Math.sqrt($('#amount').val()));
+          if (num2 == 0){
+            num2++;
+          }
+          var num3 = num1 * num2;
+          return num3+" ÷ "+num2;
+          break;
+        case 'squared':
+          var num2 = Math.floor(Math.random()*$('#amount').val());
+          if (num2 == 0){
+            num2++;
+          }
+          return num2+" x "+num2;
+        case 'sqroot':
+          var num2 = Math.floor(Math.random()*$('#amount').val());
+          if (num2 == 0){
+            num2++;
+          }
+          return num2+" ÷ "+num2;
       }
+      // return (Math.random()*$('#amount').val()).toFixed(0)+choiceSymbol+(Math.random()*$('#amount').val()).toFixed(0);
+    };
+  }
+
+  { //Testing answers if correct
+    nextLevel = function(){
+      var ask = qBank();
+      $('#questions').text(ask);
+      highScore = 0;
+      $(document).keyup(function(){
+        if ($('.input-bar').val() == eval(stripp((ask)))){
+          highScore++;
+          timeLeft+=1.3;
+          $('#questions').effect('highlight', 'faster');
+          // $('.question-block').prepend('<div id="questions">');
+          ask = qBank();
+          $('#questions').text(ask);
+          $('.input-bar').val("");
+        }
+        else {
+          console.log('web dev fail')
+        }
+      });
+    };
+  }
+
+  { //Timer function
+    $(document).on('click', '.start-button', function(){
+      timeLeft = 10;
+      $('#timer').text('START!');
+      nextLevel();
+      $('.input-bar').show();
+      $('.input-bar').select();
+      $('.start-button').hide();
+      var timer = setInterval(function(){
+        if (timeLeft > 0) {
+          $('#timer').text(timeLeft.toFixed(0));
+          console.log(timeLeft);
+          timeLeft--;
+        }
+        else {
+          clearInterval(timer);
+          $('#timer').text(highScore+"pts");
+          $('#questions').text('GAME OVER');
+          $('.start-button').text('REPLAY?');
+          $('.input-bar').hide();
+          $('.start-button').show();
+        }
+      }, 1000);
     });
   }
-  qBank();
 
 });
 
